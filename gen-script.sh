24f7e62609a8a1e581b41935dd4de66ff885385f
Chris McKenzie <kristopolous@yahoo.com>
2020-06-25 08:42:55 -0700

    using no temp file for the gen script

--- gen-script.sh
+++ gen-script.sh
@@ -6,9 +6,8 @@ rm ep1.4.pdf
 sed \
   -e "s/%GIT%/$(git describe)/g" \
   -e "s/%DATE%/$(git log -1 --format=%as)/g" \
-  ep1.4.fountain > /tmp/ep1.4
-
-afterwriting --source /tmp/ep1.4 --pdf ep1.4.pdf
+  ep1.4.fountain \
+  | afterwriting --source /dev/stdin --pdf ep1.4.pdf
 
 scp -C ep1.4.pdf ep.comphistpod.com:/var/www/episodes
 
