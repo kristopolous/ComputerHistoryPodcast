b2c2e13847a43e4e39c7bf9fa7d38d4f847ce559
Chris McKenzie <kristopolous@yahoo.com>
2020-06-25 00:58:24 -0700

    Droping the version in

--- gen-script.sh
+++ gen-script.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 pkill evince
 rm ep1.4.pdf
-afterwriting --source ep1.4.fountain --pdf ep1.4.pdf
+sed "s/%GIT%/$(git describe)/g" ep1.4.fountain > /tmp/ep1.4
+afterwriting --source /tmp/ep1.4 --pdf ep1.4.pdf
 scp -C ep1.4.pdf ep.comphistpod.com:/var/www/episodes
 evince ep1.4.pdf&
 
