d44a7e8b050eb02194517f290514b8ff6f16ea78
Chris McKenzie <kristopolous@yahoo.com>
2020-06-25 01:04:59 -0700

    script update

--- gen-script.sh
+++ gen-script.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 pkill evince
+
 rm ep1.4.pdf
-sed "s/%GIT%/$(git describe)/g" ep1.4.fountain > /tmp/ep1.4
+
+sed \
+  -e "s/%GIT%/$(git describe)/g" \
+  -e "s/%DATE%/$(git log -1 --format=%as)/g" \
+  ep1.4.fountain > /tmp/ep1.4
+
 afterwriting --source /tmp/ep1.4 --pdf ep1.4.pdf
+
 scp -C ep1.4.pdf ep.comphistpod.com:/var/www/episodes
+
 evince ep1.4.pdf&
 
